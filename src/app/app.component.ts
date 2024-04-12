21baddc358a8c458eb435848ea0e9c485ef18ed6
Igor Forner <igor@igor>
2024-04-12 11:18:17 -0300

    initial task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./cabecalho/cabecalho.component";
+import { RodapeComponent } from "./rodape/rodape.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CabecalhoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'angular-bt';
